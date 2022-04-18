dc2da914b2643de3fb9ec96ace53f768162f2566
Zoheb Iqbal <zoheb.zafar.iqbal@gmail.com>
2022-04-18 16:36:02 -0400

    commit tree 1fd2f7baaf5218b25cda5f33ac15dfa2c710d62c

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+    return cats;
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+    return cats;
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop();
+    return cats;
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+    return cats
+}
+
+function appendCat(name){
+    return [...cats, name];
+}
+
+function prependCat(name){
+    return [name, ...cats];
+}
+
+function removeLastCat(){
+    return cats.slice(0, cats.length - 1);
+}
+
+function removeFirstCat(){
+    return cats.slice(1);
+}
